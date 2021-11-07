22273e0c504a3fcbc2cec879001e0299ed5f3b3f
Ashish <ashishdev@zohomail.in>
2021-11-07 10:47:10 +0530

    Pet Clinic

--- DI/src/main/java/com/ashish/DI/services/I18nEnglishGreetingService.java
+++ DI/src/main/java/com/ashish/DI/services/I18nEnglishGreetingService.java
@@ -2,7 +2,7 @@ package com.ashish.DI.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile({"en","default"})
+@Profile("en")
 @Service("i18nService")
 public class I18nEnglishGreetingService implements RandomService{
 
