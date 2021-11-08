fcb6272f74a54cd0fcff1e7ea09089b1f573e8a5
Ashish <ashishdev@zohomail.in>
2021-11-08 10:33:54 +0530

    Added homepage and Controller

---
+++ JokesApp/src/main/java/com/Ashish/JokesApp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package com.Ashish.JokesApp.controllers;
+
+import com.Ashish.JokesApp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokesService jokesService;
+
+    @Autowired
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokesService.getJoke());
+        return "Home";
+    }
+}
