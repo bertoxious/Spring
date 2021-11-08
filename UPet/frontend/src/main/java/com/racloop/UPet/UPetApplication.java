df2c2075d1c7608a7ede4b2e6daf33c026e0e527
Ashish <ashishdev@zohomail.in>
2021-11-08 14:48:42 +0530

    Made Changes

---
+++ UPet/frontend/src/main/java/com/racloop/UPet/UPetApplication.java
@@ -0,0 +1,13 @@
+package com.racloop.UPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UPetApplication.class, args);
+	}
+
+}
